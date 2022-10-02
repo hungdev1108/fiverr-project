1e7d5ad4f3c67bee404934d249f15268b92c5c3e
hungdev1108 <hungdn11895@gmail.com>
2022-10-02 17:27:18 +0700

    Create Project

---
+++ src/components/LoginForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Login.scss";
+
+function LoginForm() {
+  return (
+    <div className="login">
+      <div className="login__title">This is test scss</div>
+    </div>
+  );
+}
+
+export default LoginForm;
