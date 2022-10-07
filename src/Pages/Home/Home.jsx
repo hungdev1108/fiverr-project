3f0a56fa8f0df4026ef637ef29cdc98eb4e9bc50
kurei01 <ku12322132@gmail.com>
2022-10-07 21:18:48 +0700

    trustBy wrapper

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,50 @@
 import React from "react";
 import HomeSlider from "./HomeSlider/HomeSlider";
+import "./Home.scss";
 
 function Home() {
-  return <>
-  <HomeSlider/>
-  </>;
+  return (
+    <div className="Home">
+      <HomeSlider />
+      <div className="trusted-by container-fluid">
+        <div className="container d-flex justify-content-center align-items-center">
+          <span>Trusted by:</span>
+          <ul className="d-flex align-items-center">
+            <li>
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/facebook.31d5f92.png"
+                alt="facebook"
+              />
+            </li>
+            <li>
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/google.517da09.png"
+                alt="Google"
+              />
+            </li>
+            <li>
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/netflix.e3ad953.png"
+                alt="NETFLIX"
+              />
+            </li>
+            <li>
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/pandg.8b7310b.png"
+                alt="P&G"
+              />
+            </li>
+            <li>
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/paypal.ec56157.png"
+                alt="PayPal"
+              />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
