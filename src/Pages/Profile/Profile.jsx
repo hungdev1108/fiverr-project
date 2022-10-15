6692c008225a6ba220d2a40c4a774b5bdb903fbb
ntanh <ku12322132@gmail.com>
2022-10-16 00:40:53 +0700

    done sellerCard in profile

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import './Profile.scss'
+import SellerCard from "./SellerCard/SellerCard";
 
 function Profile() {
-  return <div>Profile</div>;
+  return <div className="Profile">
+      <main className="container main">
+        <div className="row">
+          <section className="info-column">
+            <SellerCard/>
+          </section>
+          <section className="gigs-column"></section>
+        </div>
+      </main>
+  </div>;
 }
 
 export default Profile;
